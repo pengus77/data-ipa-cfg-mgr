a4d84f4afcd5dec440808f71754b51e59f97b362
Ashwin R C <ashwin2001achu@gmail.com>
2020-03-30 14:19:46 +0000

    data-ipa-cfg-mgr: add a simple guard

--- Android.mk
+++ Android.mk
@@ -1,7 +1,9 @@
+ifneq ($(USE_DEVICE_SPECIFIC_DATA_IPA_CFG_MGR),true)
 ifeq ($(call my-dir),$(call project-path-for,qcom-data-ipa-cfg-mgr))
 
 LOCAL_PATH := $(call my-dir)
 
 include $(call first-makefiles-under,$(LOCAL_PATH))
 
+endif
 endif
